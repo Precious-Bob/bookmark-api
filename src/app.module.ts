35667aa4bf177215e55f789e58adb90c2f985ca0
Pr3ciousXX <preciousbobmanuel501@gmail.com>
2024-05-16 17:00:14 +0100

    working on db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { BookmarksModule } from './bookmarks/bookmarks.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, BookmarksModule],
+  imports: [AuthModule, UsersModule, BookmarksModule, PrismaModule],
  
 })
 export class AppModule {}
